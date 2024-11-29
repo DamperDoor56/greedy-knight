b55528c749b9043d02b3d5dcfc0f1646c326ace7
Ludmila Lopez <ludmilalopez@Aspire-A315-58-5e017d20.localdomain>
2024-11-29 09:54:20 -0300

    feat: :sparkles: modified scenes, added a hurt visual animation each time player gets hurt

--- scenes/hurtzone.gd
+++ scenes/hurtzone.gd
@@ -1,15 +1,8 @@
 extends Area2D
 @onready var health_bar: ProgressBar = $"../../../Player/Health Bar"
+@onready var player_sprite: AnimatedSprite2D = $"../../../Player/AnimatedSprite2D"
 
 var health: int = 3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health_bar.value = health
-	
-func _take_damage():
-	health -= 1
-	health_bar.value= health
-
-func _on_body_entered(body):
-	print(body)
-	_take_damage()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,31 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+@onready var health_bar: ProgressBar = $"Health Bar"
 @onready var player_sprite = $AnimatedSprite2D
+@onready var hurt_sound: AudioStream = preload("res://assets/sounds/hurt.wav")
+@onready var audio_player = $AudioStreamPlayer2D
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var health: int = 3
+var is_taking_damage: bool = false
 
+# Take damage
+func _take_damage():
+	if is_taking_damage == true:
+		player_sprite.play("hit") 
+		#audio_player.play(hurt_sound)
+		health -= 1
+		health_bar.value = health
+		await get_tree().create_timer(0.3).timeout
+		is_taking_damage = false
 	
+func _on_hurt_zone_body_entered(body: Node2D) -> void:
+	is_taking_damage = true
+	_take_damage()
+
 func _physics_process(delta):
 	if DialogManager.is_dialog_active:
 		return
@@ -30,12 +48,12 @@ func _physics_process(delta):
 		player_sprite.flip_h = true
 	
 	# Play animations
-	if is_on_floor():
+	if is_on_floor() && is_taking_damage == false:
 		if direction == 0:
 			player_sprite.play("idle")
-		else:
+		elif is_taking_damage == false:
 			player_sprite.play("run")
-	else:
+	elif is_taking_damage == false:
 		player_sprite.play("jump")
 	# Apply movement
 	if direction:
