1f2eb47f7be95521d98d472d126abb7fe728c482
Ludmila Lopez <ludmit@outlook.com>
2024-12-03 22:45:46 -0300

    feat: :sparkles: building state logic, next, ai

--- scenes/state_machine.gd
+++ scenes/state_machine.gd
@@ -3,8 +3,17 @@ class_name StateMachine
 
 var state = null
 var previus_state = null
+var states = []
 
 @onready var parent = get_parent()
+
+
+func _physics_process(delta: float) -> void:
+	if state != null:
+		_state_logic(delta)
+		var transition = _get_transition(delta)
+		if transition != null:
+			set_state(transition)
 
 func _state_logic(delta):
 	pass
@@ -14,9 +23,19 @@ func _get_transition(delta):
 
 func _enter_state(new_state, old_state):
 	pass
+	
+func _exit_state(old_state, new_state):
+	pass
 
 func set_state(new_state):
 	previus_state = state
 	state = new_state
 	
+	if previus_state != null:
+		_exit_state(previus_state, new_state)
+	if new_state != null:
+		_enter_state(new_state, previus_state)
+		
 	
+func add_state(state_name):
+	states[state_name] = states.size()
