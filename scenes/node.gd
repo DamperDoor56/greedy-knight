faee8d75b0464046199f4998452789752e071b0b
Ludmila Lopez <ludmit@outlook.com>
2024-12-04 09:53:50 -0300

    feat: :sparkles: Advances in the enemy's state machine

--- scenes/node.gd
+++ scenes/node.gd
@@ -1,16 +1,45 @@
 extends StateMachine
 
-func _state_logic(delta):
+func _ready() -> void:
 	add_state("sleep")
 	add_state("chase")
 	add_state("attack")
 	call_deferred("set_state", states.sleep)
+
+func _state_logic(delta):
+	parent.apply_gravity(delta)
+	
+	if state != states.attack && parent._should_turn():
+		parent._turn()
+	if state == states.chase:
+		parent._chase_player()
+	else:
+		parent._stop()
+
 	
 func _get_transition(delta):
+	match state:
+		states.sleep:
+			if parent._should_chase():
+				return states.chase
+		states.chase:
+			if parent._should_sleep():
+				return states.sleep
+			elif parent._should_attack():
+				return states.attack
+		states.attack:
+				if parent.is_on_floor():
+					return states.sleep
 	return null
-
 func _enter_state(new_state, old_state):
-	pass
+	match new_state:
+		states.sleep:
+			parent.animation_player.play("rest")
+		states.chase:
+			parent.animation_player.play("chase")
+		states.attack:
+			parent.animation_player.play("attack")
+			parent.attack()
 	
 func _exit_state(old_state, new_state):
 	pass
