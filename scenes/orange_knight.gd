0a9725c60477fb4af562f6729dff4598b4f982c3
Ludmila Lopez <ludmit@outlook.com>
2025-04-20 20:19:10 -0300

    feat: :sparkles: add white knight code, is an extension of the class interactionArea

--- scenes/orange_knight.gd
+++ scenes/orange_knight.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-@onready var sprite_2d = $Sprite2D
 @onready var speech_sound = preload("res://assets/sounds/voice_sample.wav")
-@onready var knight_range: Area2D = $KnightRange
+@onready var knight_range: InteractionArea = $KnightRange
 
 const lines: Array[String] = [
 	"Hello, fellow companion",
@@ -10,13 +9,13 @@ const lines: Array[String] = [
 	"You need to collect all the coins",
 	"To open the portal",
 	"What? Is perfectly normal",
-	"Try with the coin next to me",
+	"Start with the coin next to me",
 	"Hail the emperor!",
 ]
 
 func _ready():
-		knight_range.KnightInteraction.interact = Callable(self, "_on_interact")
-
-func _on_interact():
+	knight_range.interact = Callable(self, "_on_knight_interact") 
+	
+func _on_knight_interact():
 	DialogManager.start_dialog(global_position, lines, speech_sound)
 	await DialogManager.dialog_finished
